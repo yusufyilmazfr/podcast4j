71aaf368da05e6edd86d4ebbb77f9bed7b9a209a
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2022-12-24 20:10:06 +0300

    written podcastindex stats service example.

--- src/main/java/com/github/yusufyilmazfr/podcast4j/Podcast4jApplication.java
+++ src/main/java/com/github/yusufyilmazfr/podcast4j/Podcast4jApplication.java
@@ -1,6 +1,28 @@
 package com.github.yusufyilmazfr.podcast4j;
 
+import com.github.yusufyilmazfr.podcast4j.config.Config;
+import com.github.yusufyilmazfr.podcast4j.factory.Podcast4jServiceFactory;
+import com.github.yusufyilmazfr.podcast4j.service.category.Podcast4jCategoryService;
+import com.github.yusufyilmazfr.podcast4j.service.stats.Podcast4jStatsService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Podcast4jApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+        Config config = Config.builder()
+                              .secret("HQT8wkdbb5KVMGur$$TmYvg#W6Dgph4nvQkyhV#f")
+                              .authKey("CJSXRHDXXTFGKK3DYDNB")
+                              .userAgent("SuperPodcastPlayer/1.8")
+                              .build();
+
+        Podcast4jServiceFactory factory = Podcast4jServiceFactory.with(config);
+
+        Podcast4jCategoryService categoryService = factory.getCategoryService();
+        Podcast4jStatsService statsService = factory.getStatsService();
+
+        categoryService.getAll().forEach(System.out::println);
+        System.out.println("statsService.get() = " + statsService.get());
     }
 }
