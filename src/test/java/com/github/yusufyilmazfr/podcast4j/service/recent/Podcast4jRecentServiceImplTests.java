c48859a5b4ca6d33358c02029acd1d67dfabf257
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2022-12-28 20:40:04 +0300

    getFeeds, getNewFeeds and getSoundBites methods' tests are written

--- src/test/java/com/github/yusufyilmazfr/podcast4j/service/recent/Podcast4jRecentServiceImplTests.java
+++ src/test/java/com/github/yusufyilmazfr/podcast4j/service/recent/Podcast4jRecentServiceImplTests.java
@@ -1,8 +1,13 @@
 package com.github.yusufyilmazfr.podcast4j.service.recent;
 
 import com.github.yusufyilmazfr.podcast4j.arg.service.recent.EpisodesArg;
+import com.github.yusufyilmazfr.podcast4j.arg.service.recent.FeedsArg;
+import com.github.yusufyilmazfr.podcast4j.arg.service.recent.NewFeedsArg;
 import com.github.yusufyilmazfr.podcast4j.constant.TestConfig;
 import com.github.yusufyilmazfr.podcast4j.entity.Episode;
+import com.github.yusufyilmazfr.podcast4j.entity.Feed;
+import com.github.yusufyilmazfr.podcast4j.entity.NewFeed;
+import com.github.yusufyilmazfr.podcast4j.entity.SoundBite;
 import com.github.yusufyilmazfr.podcast4j.factory.Podcast4jServiceFactory;
 import org.junit.Test;
 
@@ -16,6 +21,57 @@ public class Podcast4jRecentServiceImplTests {
 
     public Podcast4jRecentServiceImplTests() {
         serviceFactory = Podcast4jServiceFactory.with(TestConfig::getTestConfig);
+    }
+
+    @Test
+    public void getFeeds_shouldReturnMatchedFeeds() throws IOException, InterruptedException {
+        // Arrange
+        Podcast4jRecentService recentService = serviceFactory.getRecentService();
+
+        FeedsArg arg = FeedsArg.builder()
+                               .max(1)
+                               .lang("tr")
+                               .cat("technology")
+                               .build();
+
+        // Actual
+        List<Feed> feeds = recentService.getFeeds(arg);
+
+        // Assert
+        assertNotNull(feeds);
+        assertEquals(1, feeds.size());
+        assertEquals("tr", feeds.get(0).getLanguage());
+    }
+
+    @Test
+    public void getNewFeeds_shouldReturnMatchedNewFeeds() throws IOException, InterruptedException {
+        // Arrange
+        Podcast4jRecentService recentService = serviceFactory.getRecentService();
+
+        NewFeedsArg arg = NewFeedsArg.builder()
+                                     .max(1)
+                                     .build();
+
+        // Actual
+        List<NewFeed> feeds = recentService.getNewFeeds(arg);
+
+        // Assert
+        assertNotNull(feeds);
+        assertEquals(1, feeds.size());
+    }
+
+    @Test
+    public void getSoundBites_shouldReturnMatchedSoundBites() throws IOException, InterruptedException {
+        // Arrange
+        Podcast4jRecentService recentService = serviceFactory.getRecentService();
+        int expectedSize = 5;
+
+        // Actual
+        List<SoundBite> soundBites = recentService.getSoundBites(expectedSize);
+
+        // Assert
+        assertNotNull(soundBites);
+        assertEquals(expectedSize, soundBites.size());
     }
 
     @Test
