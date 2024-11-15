d6924a00da1d45fca6f8337f044d47c9721656c7
Konie <guofengzuo@hotmail.com>
2024-11-15 17:35:05 +0800

    handle deserialize exception when getting no podcast by Podcast4jPodcastService

--- src/main/java/com/github/yusufyilmazfr/podcast4j/response/PodcastResponse.java
+++ src/main/java/com/github/yusufyilmazfr/podcast4j/response/PodcastResponse.java
@@ -2,7 +2,9 @@ package com.github.yusufyilmazfr.podcast4j.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.github.yusufyilmazfr.podcast4j.entity.Podcast;
+import com.github.yusufyilmazfr.podcast4j.util.PodcastDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,5 +13,6 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PodcastResponse extends BaseResponse {
     @JsonProperty("feed")
+    @JsonDeserialize(using = PodcastDeserializer.class)
     private Podcast podcast;
 }
